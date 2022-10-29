4e5390379fd102f95f73748806692d9fefd403ce
unknown <dgoodenough@proton.me>
2022-10-29 11:15:51 -0700

    Render items to the cart, and make clear cart button work

--- browser_environment/hipster-shopping/main.js
+++ browser_environment/hipster-shopping/main.js
@@ -8,3 +8,51 @@ showCart.addEventListener('click', () => {
     cart.className = 'shopping-cart'
   }
 })
+
+let products = document.querySelector('.products')
+
+let cartArr = [];
+
+products.addEventListener('click', (e) => {
+  if(e.target.className === 'add-to-cart'){
+    let itemName = e.target.closest('.item').attributes["data-name"].value;
+    let itemPrice = e.target.closest('.item').attributes["data-price"].value;
+
+    let productObj = {name: itemName, price: itemPrice};
+
+    cartArr.push(productObj);
+    updateCart(cartArr);
+  }
+})
+
+function updateCart(arr) {
+  let productList = document.querySelector('.cart-list');
+
+  while(productList.hasChildNodes()){
+    productList.removeChild(productList.firstChild)
+  }
+  let totalPrice = 0;
+  
+  arr.forEach((item) => {
+    totalPrice += Number(item.price);
+    let itemP = document.createElement('p');
+    itemP.innerHTML = `${item.name} - $${item.price}`
+    productList.appendChild(itemP);
+  })
+
+  document.querySelector('.total').innerHTML = totalPrice;
+}
+
+let clearBtn = document.querySelector('.clear-cart')
+
+clearBtn.addEventListener('click', () => {
+  let productList = document.querySelector('.cart-list');
+
+  while(productList.hasChildNodes()){
+    productList.removeChild(productList.firstChild)
+  }
+
+  document.querySelector('.total').innerHTML = 0;
+
+  cartArr = [];
+}) 
